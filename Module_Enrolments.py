905ea5794a4d956819936bc6f7b996386b341aa4
ropeake <37979387+ropeake@users.noreply.github.com>
2018-04-11 19:23:49 +0100

    Import csv

--- Module_Enrolments.py
+++ Module_Enrolments.py
@@ -13,6 +13,5 @@ if o.DoModal()==1:
     filename=o.GetPathName()
 #%% Import CSV file
     df=pd.read_csv(filename,',',header=0,
-                   names=['ID','NAME','EMAIL','STATUS','ROUTE','MODULE_CODE','MODULE_NAME'])
-    df['Student_Number']=df.SCJ_Code.str.split('/',expand=True)[0] #split and then return column 0 (firts column)
-    #df=df[['','']]
+                   names=['ID','NAME','EMAIL','STATUS','ROUTE','LEVEL','MODULE_NAME','MODULE_CODE'])
+#
